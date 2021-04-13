4887122ead171e63d9aff8df22371c61517d53ca
namanshrimali <35870572+namanshrimali@users.noreply.github.com>
2021-04-13 10:53:56 +0530

    Updated yolo decoder to post rectify layers issue

--- models/doepd_net.py
+++ models/doepd_net.py
@@ -10,12 +10,12 @@ class DoepdNet(torch.nn.Module):
         super(DoepdNet, self).__init__()
         self.train_mode = train_mode
 
-        if self.train_mode != 'yolo' or self.train_mode != 'midas':
+        if not (self.train_mode == "yolo" or self.train_mode == "midas"):
             raise NotImplementedError(f'Current implementation does not support {train_mode} training mode')
         
         self.midas_net = MidasNet("weights/model-f6b98070.pt")
         
-        midas_encoder_filters = [256, 512, 1024, 2048] # output filters from each layer of resnext 101
+        midas_encoder_filters = [3, 256, 512, 1024, 2048] # output filters from each layer of resnext 101
                     
         # Each of the three layers in yolo takes input from last 3 layers of midas    
         self.yolo_decoder = YoloDecoder(midas_encoder_filters)
